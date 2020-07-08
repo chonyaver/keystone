c23df94fc2a2af43b4751de9356573d1518afaea
Tim Leslie <timl@thinkmill.com.au>
2020-07-08 15:33:12 +1000

    Add a Prisma adapter

--- api-tests/uniqueness/unique.test.js
+++ api-tests/uniqueness/unique.test.js
@@ -27,97 +27,103 @@ describe('Test isUnique flag for all field types', () => {
                   }),
                 testFn
               );
-            test(
-              'uniqueness is enforced over multiple mutations',
-              keystoneTestWrapper(async ({ keystone }) => {
-                const { errors } = await keystone.executeGraphQL({
-                  query: `
+            if (!['Decimal', 'MongoId'].includes(mod.name)) {
+              test(
+                'uniqueness is enforced over multiple mutations',
+                keystoneTestWrapper(async ({ keystone }) => {
+                  const { errors } = await keystone.executeGraphQL({
+                    query: `
                   mutation($data: TestCreateInput) {
                     createTest(data: $data) { id }
                   }
                 `,
-                  variables: {
-                    data: {
-                      testField: mod.exampleValue,
+                    variables: {
+                      data: {
+                        testField: mod.exampleValue,
+                      },
                     },
-                  },
-                });
-                expect(errors).toBe(undefined);
+                  });
+                  expect(errors).toBe(undefined);
 
-                const { errors: errors2 } = await keystone.executeGraphQL({
-                  query: `
+                  const { errors: errors2 } = await keystone.executeGraphQL({
+                    query: `
                   mutation($data: TestCreateInput) {
                     createTest(data: $data) { id }
                   }
                 `,
-                  variables: {
-                    data: {
-                      testField: mod.exampleValue,
+                    variables: {
+                      data: {
+                        testField: mod.exampleValue,
+                      },
                     },
-                  },
-                });
+                  });
 
-                expect(errors2).toHaveProperty('0.message');
-                expect(errors2[0].message).toEqual(
-                  expect.stringMatching(/duplicate key|to be unique/)
-                );
-              })
-            );
+                  expect(errors2).toHaveProperty('0.message');
+                  expect(errors2[0].message).toEqual(
+                    expect.stringMatching(
+                      /duplicate key|to be unique|Unique constraint failed on the fields/
+                    )
+                  );
+                })
+              );
 
-            test(
-              'uniqueness is enforced over single mutation',
-              keystoneTestWrapper(async ({ keystone }) => {
-                const { errors } = await keystone.executeGraphQL({
-                  query: `
+              test(
+                'uniqueness is enforced over single mutation',
+                keystoneTestWrapper(async ({ keystone }) => {
+                  const { errors } = await keystone.executeGraphQL({
+                    query: `
                   mutation($fooData: TestCreateInput, $barData: TestCreateInput) {
                     foo: createTest(data: $fooData) { id }
                     bar: createTest(data: $barData) { id }
                   }
                 `,
-                  variables: {
-                    fooData: {
-                      testField: mod.exampleValue,
+                    variables: {
+                      fooData: {
+                        testField: mod.exampleValue,
+                      },
+                      barData: {
+                        testField: mod.exampleValue,
+                      },
                     },
-                    barData: {
-                      testField: mod.exampleValue,
-                    },
-                  },
-                });
+                  });
 
-                expect(errors).toHaveProperty('0.message');
-                expect(errors[0].message).toEqual(
-                  expect.stringMatching(/duplicate key|to be unique/)
-                );
-              })
-            );
+                  expect(errors).toHaveProperty('0.message');
+                  expect(errors[0].message).toEqual(
+                    expect.stringMatching(
+                      /duplicate key|to be unique|Unique constraint failed on the fields/
+                    )
+                  );
+                })
+              );
 
-            test(
-              'Configuring uniqueness on one field does not affect others',
-              keystoneTestWrapper(async ({ keystone }) => {
-                const { data, errors } = await keystone.executeGraphQL({
-                  query: `
+              test(
+                'Configuring uniqueness on one field does not affect others',
+                keystoneTestWrapper(async ({ keystone }) => {
+                  const { data, errors } = await keystone.executeGraphQL({
+                    query: `
                   mutation($fooData: TestCreateInput, $barData: TestCreateInput) {
                     foo: createTest(data: $fooData) { id }
                     bar: createTest(data: $barData) { id }
                   }
                 `,
-                  variables: {
-                    fooData: {
-                      testField: mod.exampleValue,
-                      name: 'jess',
+                    variables: {
+                      fooData: {
+                        testField: mod.exampleValue,
+                        name: 'jess',
+                      },
+                      barData: {
+                        testField: mod.exampleValue2,
+                        name: 'jess',
+                      },
                     },
-                    barData: {
-                      testField: mod.exampleValue2,
-                      name: 'jess',
-                    },
-                  },
-                });
+                  });
 
-                expect(errors).toBe(undefined);
-                expect(data).toHaveProperty('foo.id');
-                expect(data).toHaveProperty('bar.id');
-              })
-            );
+                  expect(errors).toBe(undefined);
+                  expect(data).toHaveProperty('foo.id');
+                  expect(data).toHaveProperty('bar.id');
+                })
+              );
+            }
           });
         });
 
